76ae3fada47ba24a71e54ae3496a91a31d203b67
Vibhor <vibsmish@gmail.com>
2022-10-11 15:14:35 +0530

    domain change

--- Vcart/settings.py
+++ Vcart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t2866cyphvsyx-9*fu^&(62h4+)k7lwsfb==ue-enrtd9w)dzq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','https://wecart.herokuapp.com/']
 
 
 # Application definition

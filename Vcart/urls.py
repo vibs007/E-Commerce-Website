d2474b72d5cad5802f1855c909f79370e1757b7f
Vibhor <vibsmish@gmail.com>
2022-10-11 17:30:56 +0530

    App for heroku

--- Vcart/urls.py
+++ Vcart/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import imp
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,7 +20,7 @@ from django.conf.urls.static import static
 from . import views
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +28,6 @@ urlpatterns = [
     path("shop/", include('shop.urls')),
     path("blog/", include('blog.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
